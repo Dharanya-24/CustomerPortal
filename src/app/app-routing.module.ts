2427e259146dc6f20c925b407ec5a26a85b55287
Dharanya-24 <dharanyamohan434@gmail.com>
2025-07-24 18:33:08 +0530

    Add app files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SalesDataComponent } from './dashboard/sales-data/sales-data.component';
+import { InquiryDataComponent } from './dashboard/inquiry-data/inquiry-data.component';
+import { SalesOrderDataComponent } from './dashboard/sales-data/sales-order-data/sales-order-data.component';
+import { DeliveryListComponent } from './dashboard/sales-data/delivery-list/delivery-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'dashboard/sales-data', component: SalesDataComponent },
+  { path: 'dashboard/inquiry-data', component: InquiryDataComponent },
+  { path: 'dashboard/sales-data/sales-order-data', component: SalesOrderDataComponent },
+  { path: 'dashboard/sales-data/delivery', component: DeliveryListComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
