868e702046a7deafec4149adedd0b88da3caf530
Dharanya-24 <dharanyamohan434@gmail.com>
2025-07-24 18:32:14 +0530

    Add commit -m modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SalesDataComponent } from './dashboard/sales-data/sales-data.component';
+import { InquiryDataComponent } from './dashboard/inquiry-data/inquiry-data.component';
+import { SalesOrderDataComponent } from './dashboard/sales-data/sales-order-data/sales-order-data.component';
+import { DeliveryListComponent } from './dashboard/sales-data/delivery-list/delivery-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProfileComponent,
+    SalesDataComponent,
+    InquiryDataComponent,
+    SalesOrderDataComponent,
+    DeliveryListComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
